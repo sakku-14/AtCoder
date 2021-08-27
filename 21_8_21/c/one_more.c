1944bfb3173dbad53ebe7e9fd623a7c5e6aaec0a
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-08-28 06:55:28 +0900

    [Improve] c

--- 21_8_21/c/one_more.c
+++ 21_8_21/c/one_more.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main ()
 {
 	char buf[256];
 	char *s_in[2];
+	char letters[8][1];
+	char words[40320][8];
 
+	//標準入力から空白区切りで別々の文字列へ格納
 	fgets(buf, 256, stdin);
-	s_in = strtok(buf, " ");
-
-	printf();
+	s_in[0] = strtok(buf, " ");
+	s_in[1] = strtok(NULL, "\n");
 
 	return (0);
 }
