9d85e46e91da5875cd51953dd76a6d627a3f145d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-08-27 18:35:31 +0900

    [Improve] b for long long in stdin

--- 21_8_21/b/log2_n.c
+++ 21_8_21/b/log2_n.c
@@ -4,17 +4,19 @@
 int main()
 {
 	char buf[256];
-	int max;
-	int tmp = 1;
+	long long max;
+	long long tmp = 1;
 	int k = 0;
 
 	fgets(buf, 256, stdin);
-	max = atoi(buf);
+	max = atoll(buf);
 	while (tmp <= max)
 	{
 		k++;
 		tmp *= 2;
 	}
+	if (k != 0)
+		k--;
 	printf("%d\n", k);
 
 	return (0);
