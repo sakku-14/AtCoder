411b96d314610ac7de62d588a022e89ee4ff8121
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-08-29 16:38:08 +0900

    [Improve]

--- 21_8_21/c/one_more.c
+++ 21_8_21/c/one_more.c
@@ -5,13 +5,15 @@ int main ()
 {
 	char buf[256];
 	char *s_in[2];
-	char letters[8][1];
+	char letters[8];
 	char words[40320][8];
 
 	//標準入力から空白区切りで別々の文字列へ格納
 	fgets(buf, 256, stdin);
 	s_in[0] = strtok(buf, " ");
 	s_in[1] = strtok(NULL, "\n");
+
+	
 
 	return (0);
 }
